0bd799d15823b00172f9d1c30e4b2a5105e6295b
Vandana Reddy <101381073+Vandana-R-dev@users.noreply.github.com>
2025-02-21 11:21:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
