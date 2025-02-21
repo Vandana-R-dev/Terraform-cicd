ca8a54562574741fb491b896152bbd2001852212
Vandana Reddy <101381073+Vandana-R-dev@users.noreply.github.com>
2025-02-21 11:22:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  
 }
 resource "aws_instance" "public_instance" {
  ami           = var.ami
